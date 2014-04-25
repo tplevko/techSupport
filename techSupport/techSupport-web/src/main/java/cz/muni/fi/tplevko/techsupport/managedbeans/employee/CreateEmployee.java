e578cb931548ab5f46f67f0a6d7d6cf51141dfb5
tplevko <tplevko@redhat.com>
2014-04-25 18:42:41 +0200

    some admin functionality added

--- techSupport/techSupport-web/src/main/java/cz/muni/fi/tplevko/techsupport/managedbeans/employee/CreateEmployee.java
+++ techSupport/techSupport-web/src/main/java/cz/muni/fi/tplevko/techsupport/managedbeans/employee/CreateEmployee.java
@@ -2,9 +2,11 @@ package cz.muni.fi.tplevko.techsupport.managedbeans.employee;
 
 import cz.muni.fi.tplevko.techsupport.entity.dto.EmployeeDto;
 import cz.muni.fi.tplevko.techsupport.services.EmployeeService;
+import cz.muni.fi.tplevko.techsupport.utils.ShaEncoder;
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import org.apache.shiro.crypto.hash.Sha256Hash;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -17,32 +19,47 @@ import org.springframework.stereotype.Component;
 @ManagedBean
 @Scope("session")
 public class CreateEmployee implements Serializable {
-
+    
     private static final long serialVersionUID = 1L;
-
+    
     @PostConstruct
     public void init() {
-
+        
         employeeDto = new EmployeeDto();
     }
-
+    
     @Autowired
     private EmployeeService employeeService;
-
+    
     private EmployeeDto employeeDto;
-
+    
     public EmployeeDto getEmployeeDto() {
         return employeeDto;
     }
-
+    
     public void setEmployeeDto(EmployeeDto employeeDto) {
         this.employeeDto = employeeDto;
     }
+    
+    public String createEmployee() {
+        
+        String salt;
+        Sha256Hash passwordHash;
 
-    private String createEmployee() {
-
+        // TODO : zmenit...    
+        // TODO : treba spravit to generovanie hesiel pre zamestnancov
+        employeeDto.setPassword("passwd");
+        
+        employeeDto.getPassword();
+        
+        salt = ShaEncoder.generateSalt();
+        passwordHash = ShaEncoder.hash(employeeDto.getPassword(), salt);
+        
+        employeeDto.setSalt(salt);
+        employeeDto.setPassword(passwordHash.toHex());
+        
         employeeService.createEmployee(employeeDto);
-
-        return "/employee/admin/employeeList?faces-redirect=true";
+        
+        return "/employee/admin/employee/employeeList?faces-redirect=true";
     }
 }
--- techSupport/techSupport-web/src/main/java/cz/muni/fi/tplevko/techsupport/managedbeans/employee/EmployeeController.java
+++ techSupport/techSupport-web/src/main/java/cz/muni/fi/tplevko/techsupport/managedbeans/employee/EmployeeController.java
@@ -1,8 +1,10 @@
-package cz.muni.fi.tplevko.techsupport.managedbeans;
+package cz.muni.fi.tplevko.techsupport.managedbeans.employee;
 
 import cz.muni.fi.tplevko.techsupport.entity.dto.EmployeeDto;
 import cz.muni.fi.tplevko.techsupport.services.EmployeeService;
+import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -19,9 +21,15 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
-    
+
     private EmployeeDto employeeDto;
     private List<EmployeeDto> employeeList;
+
+    @PostConstruct
+    public void init() {
+
+        employeeList = new ArrayList<EmployeeDto>();
+    }
 
     public EmployeeDto getEmployeeDto() {
         return employeeDto;
@@ -32,12 +40,23 @@ public class EmployeeController {
     }
 
     public List<EmployeeDto> getEmployeeList() {
+        employeeList = employeeService.getAllEmployees();
         return employeeList;
     }
 
     public void setEmployeeList(List<EmployeeDto> employeeList) {
         this.employeeList = employeeList;
     }
-    
-    
+//    
+//    public String createEmployee() {
+//        
+//        // TODO : create automatic passwd generation... 
+//        
+//        employeeDto.setPassword("passwd");
+//        
+//        employeeService.createEmployee(employeeDto);
+//        
+//        return "";
+//    }
+
 }
