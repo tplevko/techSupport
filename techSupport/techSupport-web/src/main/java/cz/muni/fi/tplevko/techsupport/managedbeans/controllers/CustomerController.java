e3b0203d4767a89ae4e91ba05bd32457f79d6d4e
tplevko <tplevko@redhat.com>
2014-05-08 22:06:40 +0200

    customer almost finished

--- techSupport/techSupport-web/src/main/java/cz/muni/fi/tplevko/techsupport/managedbeans/controllers/CustomerController.java
+++ techSupport/techSupport-web/src/main/java/cz/muni/fi/tplevko/techsupport/managedbeans/controllers/CustomerController.java
@@ -5,8 +5,10 @@ import cz.muni.fi.tplevko.techsupport.entity.dto.CustomerDto;
 import cz.muni.fi.tplevko.techsupport.services.CustomerService;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -25,6 +27,7 @@ public class CustomerController {
 
     private CustomerDto customer;
     private List<CustomerDto> customerList;
+    private CustomerDto selectedItem;
 
     @PostConstruct
     public void init() {
@@ -59,26 +62,6 @@ public class CustomerController {
         this.customerList = customerList;
     }
 
-    public String activeteCustomer(CustomerDto customerToActivate) {
-//
-//        ProductDto newProduct = new ProductDto();
-//
-//        newProduct.setName(productName);
-//
-//        customerService.createProduct(newProduct);
-//
-//        return "/employee/admin/product/productList?faces-redirect=true";
-        return null;
-    }
-
-    public String deactivateCustomer() {
-//
-//        customerService.deleteProduct(customer);
-//
-//        return "/employee/admin/product/productList?faces-redirect=true";
-        return null;
-    }
-
     // TODO : editovanie asi nebude potrebne...
     public String deleteCustomer() {
 
@@ -86,13 +69,54 @@ public class CustomerController {
 //
 //        customerService.updateProduct(customer);
 //
+
 //        return "/product/productList?faces-redirect=true";
-        return null;
+        return "/employee/admin/customer/customerList?faces-redirect=true";
     }
     
     public String editCustomerInfo() {
         
         return null;
     }
+    
+    public String editCustomerBefore() {
+        
+        Map<String, String> parameterMap = (Map<String, String>) FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        Long customerId = Long.valueOf(parameterMap.get("customerId"));
+        customer = customerService.findCustomerById(customerId);
+
+        return "/employee/admin/customer/editCustomer?faces-redirect=true";
+    }
+
+    public CustomerDto getSelectedItem() {
+        return selectedItem;
+    }
+
+    public void setSelectedItem(CustomerDto selectedItem) {
+        this.selectedItem = selectedItem;
+    }
+    
+    public void deselect() {
+        selectedItem = null;
+    }
+
+    public void rowKeyListener(Object rowKey) {
+
+        // TODO : debug...
+        Long id = Long.valueOf((String) rowKey);
+//
+//        if (selectedItem != null
+//                && selectedItem.getId() == id) {
+//
+//            deselect();
+//        } else {
+
+//        deselect();
+        
+        selectedItem = customerService.findCustomerById(id);
+//        }
+
+    }
+
 
 }
