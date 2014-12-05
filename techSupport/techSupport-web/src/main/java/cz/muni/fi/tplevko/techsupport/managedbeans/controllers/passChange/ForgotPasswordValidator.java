f6aa71d9a6a7971c00408f52b284237f60f31e81
tplevko <tplevko@redhat.com>
2014-12-05 09:53:20 +0100

    The two classes I moved into their own package...

---
+++ techSupport/techSupport-web/src/main/java/cz/muni/fi/tplevko/techsupport/managedbeans/controllers/passChange/ForgotPasswordValidator.java
@@ -0,0 +1,48 @@
+package cz.muni.fi.tplevko.techsupport.managedbeans.controllers.passChange;
+
+import cz.muni.fi.tplevko.techsupport.entity.dto.CustomerDto;
+import cz.muni.fi.tplevko.techsupport.services.CustomerService;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author tplevko
+ */
+@Component
+@Scope("request")
+public class ForgotPasswordValidator implements Validator {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Override
+    public void validate(FacesContext context, UIComponent component,
+            Object value) throws ValidatorException {
+
+        String emailAddress = value.toString();
+
+        // Let required="true" do its job.
+        if (emailAddress == null || emailAddress.isEmpty()) {
+            return;
+        }
+
+//        CustomerDto customer = new CustomerDto();
+        CustomerDto customer = customerService.findCustomerByEmail(emailAddress);
+
+        if (customer == null) {
+            throw new ValidatorException(new FacesMessage(
+                    "email entered is not present in our records. If \n"
+                    + "you forgot your registration email, contact the \n"
+                    + "administrator please"));
+        }
+    }
+}
